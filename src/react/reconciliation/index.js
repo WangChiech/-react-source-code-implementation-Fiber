a038bff3896db5bb330e99648e6032b2ef3df315
WangChiech <wangchiech@yeah.net>
2022-08-09 20:07:36 +0800

    实现任务调度逻辑

--- src/react/reconciliation/index.js
+++ src/react/reconciliation/index.js
@@ -1,11 +1,27 @@
 import { createTaskQueue } from '../Misc'
 
 const taskQueue = createTaskQueue()
+const subTask = null
 
+const getFirstTask =() => {}
 
+const executeTask = fiber => {}
+
+const workLoop = deadline => {
+  if (!subTask) {
+    subTask = getFirstTask()
+  }
+  // 如果有更高优先级的任务，下面任务会被打断(故需要在 performTask 中的 workLoop() 后重新注册任务)
+  while (subTask && deadline.timeRemaining() > 1) {
+    subTask = executeTask(subTask)
+  }
+}
 // 只执行调度任务，不负责执行任务
 const performTask = deadline => {
   workLoop(performTask)
+  if (subTask || taskQueue.isEmpty() {
+    requestIdleCallback(performTask)
+  })
 }
 // element(jsx) 是 dom(root dom元素) 的子集
 export const render =  (element, dom) => {
