bec35649d3b02b6545a59d49cbb9ab56ffd02760
WangChiech <wangchiech@yeah.net>
2022-08-09 20:39:23 +0800

    构建根节点 fiber 对象

--- src/react/reconciliation/index.js
+++ src/react/reconciliation/index.js
@@ -1,27 +1,41 @@
 import { createTaskQueue } from '../Misc'
 
 const taskQueue = createTaskQueue()
-const subTask = null
+let subTask = null
 
-const getFirstTask =() => {}
+const getFirstTask =() => {
+  // 从任务队列中获取任务
+  const task = taskQueue.pop()
+  //返回最外层节点的 fiber 对象
+  return {
+    props: task.props, // 节点属性
+    stateNode: task.dom, // 节点 DOM 对象
+    tag: 'host_root', // 节点标记 (hostRoot | hostComponent | classComponent | functionComponent)
+    effects: [], // 存储需要更改的 fiber 对象
+    child: null // 子级 fiber
+  }
+}
 
 const executeTask = fiber => {}
 
 const workLoop = deadline => {
   if (!subTask) {
+    // 返回 fiber 对象
     subTask = getFirstTask()
+    console.log('subTask', subTask)
   }
   // 如果有更高优先级的任务，下面任务会被打断(故需要在 performTask 中的 workLoop() 后重新注册任务)
   while (subTask && deadline.timeRemaining() > 1) {
     subTask = executeTask(subTask)
+    console.log('executeTask return', subTask)
   }
 }
 // 只执行调度任务，不负责执行任务
 const performTask = deadline => {
-  workLoop(performTask)
-  if (subTask || taskQueue.isEmpty() {
+  workLoop(deadline)
+  if (subTask || taskQueue.isEmpty()) {
     requestIdleCallback(performTask)
-  })
+  }
 }
 // element(jsx) 是 dom(root dom元素) 的子集
 export const render =  (element, dom) => {
